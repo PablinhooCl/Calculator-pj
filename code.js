5a5413fe0e87f1371e89ad068f355a4cae05a4b0
pablinhoocl <xpabliito@gmail.com>
2023-09-20 16:31:35 -0700

    se avanzo en el ingreso de los operadores y numeros, la funcion de operadores deberia cambiar al momento del primer click y el segundo digito deberia agregarse con concat

--- code.js
+++ code.js
@@ -1,22 +1,90 @@
 
 let operando;
 let number1="";
-
+let numberHolder='';
+let numberHolderDos='';
 function digitAdder(x) {
     
     
     let numberOnDisplay=x;
     console.log(numberOnDisplay)
-    number1=`${number1}${numberOnDisplay}`;
-    console.log(number1)
-    document.getElementById("display").innerHTML = number1;
+    numberHolder=`${numberHolder}${numberOnDisplay}`;
+    console.log(numberHolder)
+    document.getElementById("display").innerHTML = numberHolder;
     
     }
 function operatorAdder(y) {
 //`${number1}`
-if(y='+'){
-    operando=`${number1}+`;
+if(y=='+'){
+    console.log(y);
+    number1=numberHolder
+    operando=`${numberHolderDos}+${number1}`;
     console.log(operando)
-    numberOnDisplay=0;
-}
+    numberHolder='';
+    const numeros= document.getElementsByClassName("botones numeros");
+    
+    for (let i = 0; i < numeros.length; i++) {
+        numeros[i].addEventListener("click", function() {
+          segundoNumero(numeros[i].innerHTML);
+          console.log(numeros[i]);
+        });
+      }console.log(numeros);
+}else if(y=='-'){
+    console.log(y);
+    number1=numberHolder
+    operando=`${numberHolderDos}-${number1}`;
+    console.log(operando)
+    numberHolder='';
+    
+    
+     const numeros= document.getElementsByClassName("botones numeros");
+    console.log(numeros);
+    for (let i = 0; i < numeros.length; i++) {
+        numeros[i].addEventListener("click", function() {
+          segundoNumero(numeros[i].innerHTML);
+          console.log(numeros[i]);
+        });
+      }console.log(numeros);
+}else if(y=='/'){
+    console.log(y);
+    number1=numberHolder
+    operando=`${numberHolderDos}/${number1}`;
+    console.log(operando)
+    numberHolder='';
+
+    
+     const numeros= document.getElementsByClassName("numeros");
+     console.log(numeros);
+    for (let i = 0; i < numeros.length; i++) {
+    
+        numeros[i].addEventListener("click", function() {
+          segundoNumero(numeros[i].innerHTML);
+          console.log(numeros[i]);
+        });
+      }console.log(numeros);
+    
+}else if(y=='*'){
+    console.log(y);
+    number1=numberHolder
+    operando=`${numberHolderDos}*${number1}`;
+    console.log(operando)
+    numberHolder='';
+    
+    
+     const numeros= document.getElementsByClassName("botones numeros");
+     console.log(numeros);
+    for (let i = 0; i < numeros.length; i++) {
+        numeros[i].addEventListener("click", function() {
+          segundoNumero(numeros[i].innerHTML);
+          console.log(numeros[i]);
+        });
+      }console.log(numeros);
+};}
+function segundoNumero(x){
+    let numberOnDisplay=x;
+    console.log(numberOnDisplay)
+    numberHolderDos=`${numberHolderDos}${numberOnDisplay}`;
+    console.log(numberHolderDos)
+    console.log(operando)
+    document.getElementById("display").innerHTML = numberHolderDos;    
 }
